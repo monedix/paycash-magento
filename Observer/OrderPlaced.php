711785dfa0ac9675cc638f487081ed240e53d093
e.nava <44176225+enavavirtualmx@users.noreply.github.com>
2022-02-13 05:07:19 -0600

    PayCash 1.0.34

--- Observer/OrderPlaced.php
+++ Observer/OrderPlaced.php
@@ -4,34 +4,36 @@ namespace Paycash\Pay\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
 use Psr\Log\LoggerInterface;
-use Magento\Framework\Event\Observer;
+//use Magento\Framework\Event\Observer;
 
-use Paycash\Pay\Model\Payment as Payment;
+//use Paycash\Pay\Model\Payment as Payment;
 
 class OrderPlaced implements ObserverInterface
 {
-protected $payment;
+//protected $payment;
  protected $logger;
 
- public function __construct(Payment $payment, LoggerInterface $logger) {
-     $this->payment = $payment;
+ public function __construct(/*Payment $payment, */LoggerInterface $logger) {
+    // $this->payment = $payment;
  $this->logger = $logger;
  }
 
- public function execute(Observer $observer)
+ public function execute(\Magento\Framework\Event\Observer $observer)
  {
     try 
     {
+        $this->logger->info("SE EJECUTO LA FUNCION EXECUTE 1");
         $order = $observer->getEvent()->getOrder();
 
-        $this->logger->info("SE EJECUTO LA FUNCION EXECUTE");
+        $this->logger->info("SE EJECUTO LA FUNCION EXECUTE 2");
         $total = $order->getGrandTotal();
         $this->logger->info($total);
 
-        return $this->payment->orderPlaced($order);
+        //return $this->payment->orderPlaced($order);
     }
     catch (\Exception $e)
     {
+        $this->logger->info("SE EJECUTO LA FUNCION EXECUTE CON ERROR");
         $this->logger->info($e->getMessage());
     }
  }

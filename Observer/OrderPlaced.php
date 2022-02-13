d111d0f9058e4a360c71865294297867ca0f9abe
e.nava <44176225+enavavirtualmx@users.noreply.github.com>
2022-02-13 03:46:55 -0600

    PayCash 1.0.32

---
+++ Observer/OrderPlaced.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Paycash\Pay\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Psr\Log\LoggerInterface;
+use Magento\Framework\Event\Observer;
+
+use Paycash\Pay\Model\Payment as Payment;
+
+class OrderPlaced implements ObserverInterface
+{
+protected $payment;
+ protected $logger;
+
+ public function __construct(Payment $payment, LoggerInterface$logger) {
+     $this->payment = $payment;
+ $this->logger = $logger;
+ }
+
+ public function execute(Observer $observer)
+ {
+    try 
+    {
+        $order = $observer->getEvent()->getOrder();
+
+        $total = $order->getGrandTotal();
+        $this->logger->info($total);
+
+        return $this->payment->orderPlaced($order);
+    }
+    catch (\Exception $e)
+    {
+        $this->logger->info($e->getMessage());
+    }
+ }
+}

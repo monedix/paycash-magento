e1bbf2179c35941a97e096af190085d657285963
e.nava <44176225+enavavirtualmx@users.noreply.github.com>
2022-03-23 04:13:33 -0600

    E.R.F.N V => 1.0.47

---
+++ Block/Onepage/Data.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Paycash\Pay\Helper;
+use Magento\Framework\App\Helper\Context;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Model\Session as CustomerSession;
+class Data extends AbstractHelper
+{
+    protected $customerRepository;
+    public function __construct(
+        Context $context,
+        CustomerRepositoryInterface $customerRepository,
+        CustomeSession $customerSession
+        )
+    {
+        $this->customerRepository = $customerRepository;
+        $this->customerSession = $customerSession;
+        parent::__construct($context);
+    }
+    public function getOrderNumberValue()
+    {
+        $customerId  = $this->customerSession->getCustomer()->getId();
+        $customer = $this->customerRepository->getById($customerId);
+        return $customer->getData('trade_ordernumber');
+    }
+}

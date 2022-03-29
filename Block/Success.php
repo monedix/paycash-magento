41822c91e52a89f2909e8e8c76c8b9a848f03e6b
e.nava <44176225+enavavirtualmx@users.noreply.github.com>
2022-03-29 03:56:43 -0600

    E.R.F.N V => 1.0.92

--- Block/Success.php
+++ Block/Success.php
@@ -6,6 +6,6 @@ class Success extends \Magento\Framework\View\Element\Template
 {
    public function one()
    {
-       return 'Estamos en el bloque';
+       return 'Estamos en el bloque 5848676784759494';
    }
 }
